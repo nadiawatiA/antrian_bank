9df70a1eedfe04f6db70de34eebca2a2345b44c5
nadiawatiA <62791937+nadiawatiA@users.noreply.github.com>
2020-08-06 15:01:55 +0700

    Add files via upload

---
+++ tabungan.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Antrian Bank</title>
+    <link rel="stylesheet" href="../font-awesome-4.7.0/css/font-awesome.min.css">
+
+    <!-- Bootstrap core CSS -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="../dashboard.css" rel="stylesheet">
+
+  </head>
+  
+<script type="text/javascript" src="../js/jquery-3.2.1.min.js"></script>
+<script type="text/javascript" src="../js/bootstrap.min.js"></script>
+<br><br><br>
+  <body>
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+      <a class="navbar-brand" href="#">Antrian Bank</a>
+
+
+
+    </nav>
+
+    <div class="container-fluid">
+      <div class="row">
+        <nav class="col-sm-3 col-md-2 d-none d-sm-block bg-light sidebar">
+<div class="list-group panel"><br><br><br>
+
+<a href="landingpage.php" class="list-group-item collapsed" data-parent="#sidebar"><i class="fa fa-dashboard"></i> <span class="hidden-sm-down">Beranda</span></a>
+<a href="index.php" class="list-group-item collapsed" data-parent="#sidebar"><i class="fa fa-user-circle-o"></i> <span class="hidden-sm-down">Keluar</span></a>
+            </div>
+          
+        </nav>
+
+<main class="col-sm-9 ml-sm-auto col-md-10 pt-3" role="main">
+
+
+<div class="card">
+  <div class="card-header">
+    Buka Rekening
+  </div>
+  <div class="card-body">
+    <h4 class="card-title">Buka Rekening</h4>
+  <ul class="list-group">
+  <li class="list-group-item active">Persyaratan</li>
+  <li class="list-group-item">Mengisi formulir aplikasi pembukaan rekening</li>
+  <li class="list-group-item">Setoran awal sebesar Rp 100.000,-</li>
+  <li class="list-group-item">Perorangan
+Melengkapi identitas diri
+WNI : KTP / SIM / Paspor
+WNA : KITAS atau KITAP</li>
+  <li class="list-group-item">Non Perorangan
+Melengkapi identitas perusahaan
+Anggaran Dasar
+SIUP, SITU, dsb</li>
+</ul>
+
+  </div>
+</div>
+</main>
+
+
+  </body>
+</html>
